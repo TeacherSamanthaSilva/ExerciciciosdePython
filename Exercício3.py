9024980d36523af4e77feb09febed6fff58c3aba
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2024-05-16 15:25:41 -0300

    Create Exercício3.py
    Nesse commit eu resolvi o 3 exercício da lista de exercícios em python que consiste em criar um programa que calcule uma equação do segunda grau

---
+++ Exercício3.py
@@ -0,0 +1,29 @@
+""" Escreva um programa que resolva uma equação de segundo grau. """
+
+import math
+
+def calcular_raizes(a, b, c):
+    discriminante = b**2 - 4*a*c
+
+    if discriminante > 0:
+        raiz1 = (-b + math.sqrt(discriminante)) / (2*a)
+        raiz2 = (-b - math.sqrt(discriminante)) / (2*a)
+        return raiz1, raiz2
+    elif discriminante == 0:
+        raiz = -b / (2*a)
+        return raiz, raiz
+    else:
+        parte_real = -b / (2*a)
+        parte_imaginaria = math.sqrt(abs(discriminante)) / (2*a)
+        raiz1 = complex(parte_real, parte_imaginaria)
+        raiz2 = complex(parte_real, -parte_imaginaria)
+        return raiz1, raiz2
+
+# Exemplo de uso
+a = float(input("Digite o coeficiente 'a': "))
+b = float(input("Digite o coeficiente 'b': "))
+c = float(input("Digite o coeficiente 'c': "))
+
+raizes = calcular_raizes(a, b, c)
+
+print("As raízes da equação são:", raizes)
